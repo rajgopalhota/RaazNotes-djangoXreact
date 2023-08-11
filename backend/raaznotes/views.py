1a1e9953de00451d1607a0c5807d6ca513567f50
rajgopalhota <rajgopalhota@gmail.com>
2023-08-11 15:06:15 +0530

    frontend ready

--- backend/raaznotes/views.py
+++ backend/raaznotes/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the raaznotes index.")
+    return JsonResponse({1:"raja"})
